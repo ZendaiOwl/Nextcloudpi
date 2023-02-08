fc2fbc54b16f6b8f1bd600b85fb87a6fbc029be6
Victor-ray, S <12261439+ZendaiOwl@users.noreply.github.com>
2023-02-09 00:40:10 +0100

    ࿓❯ Update
    Signed-off-by: Victor-ray, S <12261439+ZendaiOwl@users.noreply.github.com>

--- lamp.sh
+++ lamp.sh
@@ -239,7 +239,7 @@ function install
         --gecos "systemd Resolver" systemd-resolve
     fi
     installWithShadowWorkaround --no-install-recommends systemd
-    installPKG php"$PHPVER" php"$PHPVER"-{curl,gd,fpm,cli,opcache,mbstring,xml,zip,fileinfo,ldap,intl,bz2, mysql}
+    installPKG php"$PHPVER" php"$PHPVER"-{curl,gd,fpm,cli,opcache,mbstring,xml,zip,fileinfo,ldap,intl,bz2,mysql}
 
     #installPKG -t "$RELEASE" php"$PHPVER" php"$PHPVER"-curl php"$PHPVER"-gd php"$PHPVER"-fpm php"$PHPVER"-cli php"$PHPVER"-opcache \
                              php"$PHPVER"-mbstring php"$PHPVER"-xml php"$PHPVER"-zip php"$PHPVER"-fileinfo php"$PHPVER"-ldap \
