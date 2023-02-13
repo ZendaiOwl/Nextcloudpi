e83e02426e3d3d5a6d916957145ccfa34776cb4b
Victor-ray, S <12261439+ZendaiOwl@users.noreply.github.com>
2023-02-13 01:23:54 +0100

    ࿓❯ Update
    Signed-off-by: Victor-ray, S <12261439+ZendaiOwl@users.noreply.github.com>

--- etc/library.sh
+++ etc/library.sh
@@ -1012,11 +1012,11 @@ function install_template {
   fi
   {
     if [[ "${3:-}" == "--defaults" ]]; then
-      { bash "/usr/local/etc/ncp-templates/$TEMPLATE" --defaults > "$TARGET"; } 2>&1
+      { bash -c "/usr/local/etc/ncp-templates/$TEMPLATE --defaults" > "$TARGET"; } 2>&1
     else
-      { bash "/usr/local/etc/ncp-templates/$TEMPLATE" > "$TARGET"; } 2>&1 || \
+      { bash -c "/usr/local/etc/ncp-templates/$TEMPLATE" > "$TARGET"; } 2>&1 || \
         if [[ "${3:-}" == "--allow-fallback" ]]; then
-          { bash "/usr/local/etc/ncp-templates/$TEMPLATE" --defaults > "$TARGET"; } 2>&1
+          { bash -c "/usr/local/etc/ncp-templates/$TEMPLATE --defaults" > "$TARGET"; } 2>&1
         fi
     fi
   } || {
